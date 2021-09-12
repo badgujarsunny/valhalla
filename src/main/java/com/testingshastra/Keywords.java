7de8b501da26f81a7df37fe5630d15ca5b3425fd
mayur <mayuringole53@gmail.com>
2021-09-12 10:27:40 +0530

    Adding Keywords.java

---
+++ src/main/java/com/testingshastra/Keywords.java
@@ -0,0 +1,15 @@
+package com.testingshastra;
+
+import org.openqa.selenium.WebElement;
+
+public class Keywords {
+	/**
+	 * This method will click on Element
+	 * 
+	 * @param element
+	 */
+	public void clickOnElement(WebElement element) {
+		element.click();
+	}
+
+}
